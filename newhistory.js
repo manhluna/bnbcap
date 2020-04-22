5e296e621836526c5f398821505c08f5ff62fa14
johnnguyen-nodejs <hotro.dacsanhuousao@gmail.com>
2020-04-22 19:27:48 +0700

    add

--- newhistory.js
+++ newhistory.js
@@ -7,7 +7,7 @@ const newhis1 = {
     hash: "dc574aa5205a817ac4ce446197de9a894392d12e63d9f61257900523c5e1a7a3",
     value: 0.2378,
     price: 6961.56,
-    date: 1587556093647
+    date: 1587573900000
 }
 const newhis2 = {
     type: "deposit",
@@ -15,7 +15,7 @@ const newhis2 = {
     hash: "4b1099c1cbbfa4cfe218ef2c5c9dc37f6377fc0681a3c9a8f930a3ca36839dea",
     value: 0.182,
     price: 6963.41,
-    date: 1587556123529
+    date: 1587573240000
 }
 // const newadd = async () => {
 //     await db.user({"info.email": "hoangvanbinh029@gmail.com" },{$push: {'history': newhis1}})
@@ -23,8 +23,8 @@ const newhis2 = {
 //     await db.user({"info.email": "hoangvanbinh029@gmail.com", 'currency.symbol': newhis1.symbol}, {$inc: {'currency.$.balance': + (newhis1.value + newhis2.value )}})
 // }
 const newadd = async () => {
-    await db.user({"info.email": "hoangvanbinh029@gmail.com", 'history.date': 1587556093647},{$set: {'history.date': 1587573900000}})
-    await db.user({"info.email": "hoangvanbinh029@gmail.com", 'history.date': 1587556123529 },{$set: {'history.date': 1587573240000}})
+    await db.user({"info.email": "hoangvanbinh029@gmail.com", 'history.date': 1587556093647},{$set: {'history': newhis1}})
+    await db.user({"info.email": "hoangvanbinh029@gmail.com", 'history.date': 1587556123529 },{$set: {'history': newhis2}})
 }
 
 newadd()
